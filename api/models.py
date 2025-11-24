516421d28c7c0006d7d1aaf87c73c72d4d2ef538
IbragimKhaniev <newinterny@gmail.com>
2025-11-24 15:09:50 +0300

    Add Member, Message and Token models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,68 @@
+import secrets
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your models here.
+
+class Member(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.username
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class Message(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='messages')
+    text = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"{self.member.username}: {self.text[:50]}"
+
+
+class Token(models.Model):
+    member = models.OneToOneField(Member, on_delete=models.CASCADE, related_name='auth_token')
+    key = models.CharField(max_length=40, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.key
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_key():
+        return secrets.token_hex(20)
